a1b70a73fe3a6909abe1d99923767fe4b43be40f
Aleksandr <aleksandr.k.kruglyakov@yandex.ru>
2023-06-06 14:25:03 +0300

    add constants

---
+++ shared/constants/APPRouterPaths.ts
@@ -0,0 +1,17 @@
+export enum APP_ROUTER_PATHS {
+  MAIN = '/',
+  FAQ = '/faq',
+  STRATEGIES = '/strategies',
+  SUPPORT = '/support',
+  PRIVACY = '/privacy',
+  USER_ACCOUNTS = '/user/accounts',
+  USER_BILLS = '/user/bills',
+  USER_FAQ = '/user/faq',
+  USER_SUPPORT = '/user/support',
+  USER_SETTINGS = '/user/settings',
+  USER_STRATEGIES = '/user/strategies',
+  ADMIN_ACCOUNTS = '/admin/accounts',
+  ADMIN_STRATEGIES = '/admin/strategies',
+  ADMIN_PAYMENTS = '/admin/payments',
+  ADMIN_SETTINGS = '/admin/settings',
+}
---
+++ shared/constants/index.ts
@@ -0,0 +1,4 @@
+import { APP_ROUTER_PATHS } from "./APPRouterPaths";
+import { COLOR_THEME } from "./ColorThemes";
+
+export {APP_ROUTER_PATHS, COLOR_THEME}
