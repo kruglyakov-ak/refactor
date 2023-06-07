b01976a6bff41d9956a3fa5eb5929b210e518bf0
Aleksandr <aleksandr.k.kruglyakov@yandex.ru>
2023-06-07 13:44:08 +0300

    add use clients components

--- features/LandingHeader/LandingHeader.tsx
+++ features/LandingHeader/LandingHeader.tsx
@@ -1,20 +1,23 @@
 import { FC } from 'react'
 import cn from 'classnames'
-
+import { usePathname } from 'next/navigation'
 import { LandingNavigation } from 'features'
 
 import { Header, MainLogo } from 'components'
 
 import s from './landingHeader.module.scss'
+import { APP_ROUTER_PATHS } from 'shared/constants'
 
 interface ILandingHeaderProps {
   className?: string
 }
 
 export const LandingHeader: FC<ILandingHeaderProps> = ({ className }) => {
+  const pathname = usePathname()
+
   return (
     <Header className={cn(s.header, className)}>
-      <MainLogo className={s.logo} />
+      {pathname !== APP_ROUTER_PATHS.MAIN && <MainLogo className={s.logo} />}
       <LandingNavigation />
     </Header>
   )
